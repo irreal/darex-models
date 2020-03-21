bf8562a052b52a2546e6c5bcae610eeef24ae18d
Milos Spasojevic <milos.spasojevic.tms@gmail.com>
2020-03-21 21:11:36 +0100

    Add first model

--- src/business-partner.ts
+++ src/business-partner.ts
@@ -1,5 +1,19 @@
 export class BusinessPartner {
+    id: number = 0;
+    registrationNumber: number = 0;
     name: string = "";
-    id: number = 1;
+    placeId: number = 0;
+    address: string = "";
+    organisationalUnit: number = 0;
+    dateAdded?: Date;
+    employeeId: number = 0;
+    deleteDate?: Date;
+    deletedByEmployee?: number;
+    additionalName?: string;
+    taxId: string = "";
     vat: string = "";
+    internalNote?: string;
+    mailingAddress?: string;
+    email?: string;
+    languageId?: number;
 }
