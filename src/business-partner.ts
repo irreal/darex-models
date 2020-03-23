ce29c381103feb1579b0f9e9e9ccdc3eb2eb7514
Milos Spasojevic <milos.spasojevic.tms@gmail.com>
2020-03-23 17:06:52 +0100

    Add decorator mapper, decorate model

--- src/business-partner.ts
+++ src/business-partner.ts
@@ -1,19 +1,54 @@
+import { field } from "@irreal/decorator-mapper";
+
 export class BusinessPartner {
+  @field("POSLOVNI_PARTNER#")
   id: number = 0;
+
+  @field("MATICNI_BROJ")
   registrationNumber: number = 0;
+
+  @field("NAZIV")
   name: string = "";
+
+  @field("ID_MESTA")
   placeId: number = 0;
+
+  @field("ADRESA")
   address: string = "";
+
+  @field("ORGANIZACIONA_JEDINICA#")
   organisationalUnit: number = 0;
+
+  @field("DATUM_OTVARANJA")
   dateAdded?: Date;
+
+  @field("RADNIK#")
   employeeId: number = 0;
+
+  @field("DATUM_BRISANJA")
   deleteDate?: Date;
+
+  @field("OBRISAO_RADNIK")
   deletedByEmployee?: number;
+
+  @field("DODATNI_NAZIV")
   additionalName?: string;
+
+  @field("PIB")
   taxId: string = "";
+
+  @field("PDV")
   vat: string = "";
+
+  @field("INT_NAPOMENA")
   internalNote?: string;
+
+  @field("ADR_ZA_SLANJE_POSTE")
   mailingAddress?: string;
+
+  @field("EMAIL")
   email?: string;
+
+  @field("JEZIK#")
   languageId?: number;
 }
